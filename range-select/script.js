7736fc0329b4acb9e0e23c2e1538bb50a5f114f2
Tigran Aginyan <taginyan91@gmail.comm>
2023-03-04 19:03:35 +0400

    improvement of range-reset task solution

--- range-select/script.js
+++ range-select/script.js
@@ -1,53 +1,63 @@
-const todo = () => {
-	const containerItemsObj = document.getElementsByClassName("container__item")
-	const containerItems = Array.from(containerItemsObj)
+const containerItemsObj = document.getElementsByClassName("container__item")
+const containerItems = Array.from(containerItemsObj)
 
-	let counterOfClicks = 0
+let counterOfClicks = 0
 
-	const clickOnItem = (e) => {
-		counterOfClicks++
-		if (counterOfClicks === 1) {
-			containerItemBgColor[e.target.id] = "red"
-			containerItems.forEach(
-				(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
-			)
-		}
-		if (counterOfClicks === 2) {
-			containerItemBgColor.forEach((item, i) => {
-				if (item === "red" && i < e.target.id) {
-					for (let j = i; j <= e.target.id; j++) {
-						containerItemBgColor[j] = "red"
-					}
-				}
-				if (item === "red" && i > e.target.id) {
-					for (let j = e.target.id; j <= i; j++) {
-						containerItemBgColor[j] = "red"
-					}
-				}
-			})
-			containerItems.forEach(
-				(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
-			)
-
-			for (let i = 0; i < containerItemBgColor.length; i++) {
-				containerItemBgColor[i] = "green"
-			}
-		}
-		if (counterOfClicks > 2) {
-			counterOfClicks = 0
-			console.log(containerItemBgColor)
-			clickOnItem(e, todo)
-		}
-	}
-
-	containerItems.forEach((item) => {
-		item.style.backgroundColor = "green"
-		item.addEventListener("click", clickOnItem)
-	})
-
-	const containerItemBgColor = containerItems.map(
-		(item) => item.style.backgroundColor
+const setColorsToItems = () => {
+	containerItems.forEach(
+		(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
 	)
 }
 
-todo()
+const setColorsToRight = (item1, item2) => {
+	for (let j = item1; j <= item2; j++) {
+		containerItemBgColor[j] = "red"
+	}
+}
+
+const setColorsToLeft = (item1, item2) => {
+	for (let j = item2; j <= item1; j++) {
+		containerItemBgColor[j] = "red"
+	}
+}
+
+const resetColors = () => {
+	for (let i = 0; i < containerItemBgColor.length; i++) {
+		containerItemBgColor[i] = "green"
+	}
+}
+
+const clickOnItem = (e) => {
+	counterOfClicks++
+	if (counterOfClicks === 1) {
+		containerItemBgColor[e.target.id] = "red"
+		setColorsToItems()
+	}
+	if (counterOfClicks === 2) {
+		containerItemBgColor.forEach((item, i) => {
+			const firstItem = i
+			const secondItem = e.target.id
+			if (item === "red" && firstItem < secondItem) {
+				setColorsToRight(firstItem, secondItem)
+			}
+			if (item === "red" && firstItem > secondItem) {
+				setColorsToLeft(firstItem, secondItem)
+			}
+		})
+		setColorsToItems()
+		resetColors()
+	}
+	if (counterOfClicks > 2) {
+		counterOfClicks = 0
+		clickOnItem(e)
+	}
+}
+
+containerItems.forEach((item) => {
+	item.style.backgroundColor = "green"
+	item.addEventListener("click", clickOnItem)
+})
+
+const containerItemBgColor = containerItems.map(
+	(item) => item.style.backgroundColor
+)
