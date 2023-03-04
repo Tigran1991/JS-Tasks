4513fb667645bda7b6b5b5ba02bdf249499127e0
Tigran Aginyan <taginyan91@gmail.comm>
2023-03-04 17:15:28 +0400

    fix reverse color changing

--- range-select/script.js
+++ range-select/script.js
@@ -1,39 +1,46 @@
-const containerItemsObj = document.getElementsByClassName("container__item")
-const containerItems = Array.from(containerItemsObj)
+const todo = () => {
+	const containerItemsObj = document.getElementsByClassName("container__item")
+	const containerItems = Array.from(containerItemsObj)
 
-let counterOfClicks = 0
+	let counterOfClicks = 0
 
-const clickOnItem = (e) => {
-	counterOfClicks++
-	if (counterOfClicks === 1) {
-		containerItemBgColor[e.target.id] = "red"
-		containerItems.forEach(
-			(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
-		)
-	}
-	if (counterOfClicks === 2) {
-		containerItemBgColor.forEach((item, i) => {
-			if (item === "red") {
-				for (let j = i; j <= e.target.id; j++) {
-					containerItemBgColor[j] = "red"
+	const clickOnItem = (e) => {
+		counterOfClicks++
+		if (counterOfClicks === 1) {
+			containerItemBgColor[e.target.id] = "red"
+			containerItems.forEach(
+				(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
+			)
+		}
+		if (counterOfClicks === 2) {
+			containerItemBgColor.forEach((item, i) => {
+				if (item === "red") {
+					if (i < e.target.id) {
+						for (let j = i; j <= e.target.id; j++) {
+							containerItemBgColor[j] = "red"
+						}
+					}
+					if (i > e.target.id) {
+						for (let j = e.target.id; j <= i; j++) {
+							containerItemBgColor[j] = "red"
+						}
+					}
 				}
-			}
-		})
-		containerItems.forEach(
-			(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
-		)
+			})
+			containerItems.forEach(
+				(item, i) => (item.style.backgroundColor = containerItemBgColor[i])
+			)
+		}
 	}
+
+	containerItems.forEach((item) => {
+		item.style.backgroundColor = "green"
+		item.addEventListener("click", clickOnItem)
+	})
+
+	const containerItemBgColor = containerItems.map(
+		(item) => item.style.backgroundColor
+	)
 }
 
-containerItems.forEach((item) => {
-	item.style.backgroundColor = "green"
-	item.addEventListener("click", clickOnItem)
-})
-
-console.log(containerItems)
-
-const containerItemBgColor = containerItems.map(
-	(item) => item.style.backgroundColor
-)
-
-console.log(containerItemBgColor)
+todo()
