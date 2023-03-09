6fb1b4759bc57322df0d1153b31300a469ce96f1
Tigran Aginyan <taginyan91@gmail.comm>
2023-03-09 06:16:30 +0400

    improving move with arrows task solutions

--- move-with-arrows/script.js
+++ move-with-arrows/script.js
@@ -4,51 +4,36 @@ const directionBottom = document.querySelector(".container__direction-bottom")
 const element = document.querySelector(".container__element")
 const rect = element.getBoundingClientRect()
 
-const directions = []
 const keysdown = {}
 
-window.addEventListener(
-	"keydown",
-	function (e) {
-		console.log(e.which)
-		keysdown[e.which] = true
-		if (keysdown["38"] === true && keysdown["37"] === true) {
-			rect.x -= 1
-			rect.y -= 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["38"] === true && keysdown["39"] === true) {
-			rect.x += 1
-			rect.y -= 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["40"] === true && keysdown["37"] === true) {
-			rect.x -= 1
-			rect.y += 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["40"] === true && keysdown["39"] === true) {
-			rect.x += 1
-			rect.y += 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["39"] === true) {
-			rect.x += 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["40"] === true) {
-			rect.y += 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["37"] === true) {
-			rect.x -= 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		} else if (keysdown["38"] === true) {
-			rect.y -= 1
-			element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-		}
-	},
-	false
-)
+const moveElement = (e) => {
+	keysdown[e.which] = true
+	if (keysdown["38"] && keysdown["37"]) {
+		rect.x -= 1
+		rect.y -= 1
+	} else if (keysdown["38"] && keysdown["39"]) {
+		rect.x += 1
+		rect.y -= 1
+	} else if (keysdown["40"] && keysdown["37"]) {
+		rect.x -= 1
+		rect.y += 1
+	} else if (keysdown["40"] && keysdown["39"]) {
+		rect.x += 1
+		rect.y += 1
+	} else if (keysdown["39"]) {
+		rect.x += 1
+	} else if (keysdown["40"]) {
+		rect.y += 1
+	} else if (keysdown["37"]) {
+		rect.x -= 1
+	} else if (keysdown["38"]) {
+		rect.y -= 1
+	}
+	element.style.transform = `translate(${rect.x}px, ${rect.y}px)`
+}
 
-window.addEventListener(
-	"keyup",
-	function (e) {
-		keysdown[e.which] = false
-	},
-	false
-)
+window.addEventListener("keydown", moveElement, false)
+
+const setKeyUp = (e) => (keysdown[e.which] = false)
+
+window.addEventListener("keyup", setKeyUp, false)
