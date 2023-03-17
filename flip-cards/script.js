0761fb8666b1515f4a3d9618cc0801ed2c23754d
Tigran Aginyan <taginyan91@gmail.comm>
2023-03-17 07:04:01 +0400

    fix flip-cards task

--- flip-cards/script.js
+++ flip-cards/script.js
@@ -6,22 +6,25 @@ const input = document.querySelector(".form__input")
 const startGame = (e) => {
 	e.preventDefault()
 
+	const MIN_RANGE = 1
+	const range = Number(input.value) + 1
 	const countOfCards = input.value * 2
 	const cardNumbers = []
 
+	const filterRepetitiveNumbers = (number) =>
+		cardNumbers.filter((item) => item === number)
+
 	const generateRandomNumber = () => {
-		const generatedNumber = utils.getRandomIndex(1, 4)
+		const generatedNumber = utils.getRandomNumber(MIN_RANGE, range)
 		if (cardNumbers.includes(generatedNumber)) {
-			const filteredNumbers = cardNumbers.filter(
-				(item) => item === generatedNumber
-			)
+			const filteredNumbers = filterRepetitiveNumbers(generatedNumber)
 			if (filteredNumbers.length < 2) {
-				cardNumbers.push(generatedNumber)
+				return cardNumbers.push(generatedNumber)
 			} else {
-				generateRandomNumber()
+				return generateRandomNumber()
 			}
 		} else {
-			cardNumbers.push(generatedNumber)
+			return cardNumbers.push(generatedNumber)
 		}
 	}
 
@@ -34,39 +37,49 @@ const startGame = (e) => {
 
 	const cards = generateRandomNumbersInRangeForCards()
 
-	const createCardElements = () => {
-		const card = document.createElement("div")
-		card.setAttribute("class", "card")
-		return card
-	}
+	const todo = []
 
-	const createFrontElements = () => {
-		const card = document.createElement("div")
-		card.setAttribute("class", "thefront")
-		return card
-	}
-
-	const createBackElements = (text) => {
-		const card = document.createElement("div")
-		card.setAttribute("class", "theback")
-		const number = document.createElement("p")
-		number.innerText = text
-		card.appendChild(number)
-		return card
-	}
+	console.log(cardNumbers)
 
 	const addCards = () => {
-		cards.forEach((card) => {
-			const cardElement = createCardElements()
-			const cardFrontElement = createFrontElements()
-			const cardBackElement = createBackElements(card)
-			cardsContainer.appendChild(cardElement)
-			cardElement.appendChild(cardFrontElement)
-			cardElement.appendChild(cardBackElement)
-			cardElement.addEventListener(
-				"click",
-				() => (cardElement.style.transform = "rotateY(180deg)")
-			)
+		cards.forEach((item, i) => {
+			const card = utils.createCard(i)
+			const cardFront = utils.createCardFront()
+			const cardBack = utils.createCardBack(item)
+			cardsContainer.appendChild(card)
+			card.appendChild(cardFront)
+			card.appendChild(cardBack)
+			card.addEventListener("click", (e) => {
+				card.style.transform = "rotateY(180deg)"
+				todo.push(e.target.parentNode.id)
+				if (todo.length === 2) {
+					const index1 = Number(todo[0])
+					const index2 = Number(todo[1])
+					console.log(cardNumbers[index1])
+					console.log(cardNumbers[index2])
+					const elements = Array.from(
+						document.getElementsByClassName("theback")
+					)
+
+					if (cardNumbers[index1] === cardNumbers[index2]) {
+						console.log(elements[index1])
+						elements[index1].style.backgroundColor = "#fff"
+						elements[index1].style.color = "#333"
+						elements[index2].style.backgroundColor = "#fff"
+						elements[index2].style.color = "#333"
+						todo.length = 0
+					} else {
+						const elements = Array.from(document.getElementsByClassName("card"))
+						console.log(elements[index1])
+						console.log(elements[index2])
+						setTimeout(() => {
+							elements[index1].style.transform = "rotateY(0deg)"
+							elements[index2].style.transform = "rotateY(0deg)"
+						}, 1000)
+						todo.length = 0
+					}
+				}
+			})
 		})
 
 		cardsContainer.style.width = `${countOfCards * 150}px`
--- flip-cards/utils.js
+++ flip-cards/utils.js
@@ -1,3 +1,25 @@
 const utils = {
-	getRandomIndex: (min, max) => Math.floor(Math.random() * (max - min)) + min,
+	getRandomNumber: (min, max) => Math.floor(Math.random() * (max - min)) + min,
+
+	createCard: (id) => {
+		const card = document.createElement("div")
+		card.setAttribute("class", "card")
+		card.setAttribute("id", id)
+		return card
+	},
+
+	createCardFront: () => {
+		const card = document.createElement("div")
+		card.setAttribute("class", "thefront")
+		return card
+	},
+
+	createCardBack: (text) => {
+		const card = document.createElement("div")
+		card.setAttribute("class", "theback")
+		const number = document.createElement("p")
+		number.innerText = text
+		card.appendChild(number)
+		return card
+	},
 }
